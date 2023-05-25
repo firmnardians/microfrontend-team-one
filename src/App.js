41f864a96bb77672814f63934626dffa8223905a
firmnardians <4647804-firmnardians@users.noreply.gitlab.com>
2023-05-25 11:26:35 +0700

    fix: add content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { lazy, Suspense } from 'react';
-import './components/styles.css';
-
-const FirstApp = lazy(() => import('FIRST_APP/app'));
+import React from 'react';
+import './assets/styles.css';
 
 const App = () => {
-	const [name, setName] = React.useState(null);
-
 	return (
-		<div className='App'>
-			<h1>This is second app</h1>
-			<h2>Micro host app is integrated here</h2>
-			{name ? <p>Your name is: {name}</p> : null}
-			<div>
-				<Suspense fallback={<span>Loading...</span>}>
-					<FirstApp onChange={(e) => setName(e.target.value)} />
-				</Suspense>
-			</div>
+		<div className='content-team-one'>
+			<h1>TEAM ONE REPOSITORY</h1>
+			<ol>
+				<li>Module A</li>
+				<li>Module B</li>
+				<li>Module C</li>
+				<li>Module D</li>
+			</ol>
 		</div>
 	);
 };
